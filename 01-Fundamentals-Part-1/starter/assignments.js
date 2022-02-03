06fef070993eb4194279a2ce60314d5a26337db0
Jameson Wang <jamesonw10@gmail.com>
2022-02-03 18:36:29 -0500

    values and variables assignment complete

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+const country = "United States of America";
+const continent = "North America";
+let population = 331;
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/index.js
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -10,9 +10,14 @@ console.log(message);
 console.log(message);
 console.log(message);
 
+/////////////////////////////////////////////////////////////
+
 // Symbols that are allowed in JS are numbers (cannot start with a number), letters, underscore _ , and $
 // Reserved names that cannot be used are "function" and "new". Must start with a underscore or a dollar sign.
 let $function = 24;
 let $new = "my gaming computer";
 
 let bing_bong = "Ling long";
+
+// Convention that are reserved that'll never change
+let PI = 3.14159;
