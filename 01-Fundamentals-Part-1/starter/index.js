d16c6e041a394135e3f1a99e2acd274b82cab7af
Jameson Wang <jamesonw10@gmail.com>
2022-02-03 18:12:44 -0500

    update

--- 01-Fundamentals-Part-1/starter/index.js
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -4,5 +4,15 @@ console.log(40 + 8 + 23 - 10);
 console.log("Jameson");
 console.log(23);
 
-let firstName = "Bob";
-console.log(firstName);
+let message = "Hello world";
+
+console.log(message);
+console.log(message);
+console.log(message);
+
+// Symbols that are allowed in JS are numbers (cannot start with a number), letters, underscore _ , and $
+// Reserved names that cannot be used are "function" and "new". Must start with a underscore or a dollar sign.
+let $function = 24;
+let $new = "my gaming computer";
+
+let bing_bong = "Ling long";
